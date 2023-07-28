ef2bab37754ab3482162dafc8595a2cfba8176f3
bbo <bbonche@henix.fr>
2023-07-28 16:22:39 +0200

    add Offset in datetime pattern

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -152,7 +152,7 @@ def type_matching(child):
     if typeName == 'date':
         return 'string', r'\d{4}-\d{2}-\d{2}'
     elif typeName == 'datetime':
-        return 'string', r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}'
+        return 'string', r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[\-+]\d{2}:\d{2}'
     elif typeName == 'phoneNumber':
         return 'string', r'tel:([#\+\*]|37000|00+)?[0-9]{2,15}'
     else:
