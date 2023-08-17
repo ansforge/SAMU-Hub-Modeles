a4ce24df65fd1cef0cf2eb5a6b437328dffa9f93
bbo <bbonche@henix.fr>
2023-08-17 17:56:13 +0200

    add Generic Message to allow inconsistent impl for now

--- src/main/java/com/hubsante/model/edxl/UseCaseMessage.java
+++ src/main/java/com/hubsante/model/edxl/UseCaseMessage.java
@@ -2,13 +2,19 @@ package com.hubsante.model.edxl;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.hubsante.model.GenericMessage;
 import com.hubsante.model.cisu.*;
 import com.hubsante.model.emsi.Emsi;
+
+import java.util.HashMap;
+
 @JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
 @JsonSubTypes({
         @JsonSubTypes.Type(CreateCaseMessage.class),
         @JsonSubTypes.Type(GenericAckMessage.class),
-        @JsonSubTypes.Type(Emsi.class)
+        @JsonSubTypes.Type(Emsi.class),
+        @JsonSubTypes.Type(GenericMessage.class)
 })
 public class UseCaseMessage {
 }
