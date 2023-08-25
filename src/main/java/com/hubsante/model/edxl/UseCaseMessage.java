7444d003ea82643a4581cec9abe47359f28dd73b
bbo <bbonche@henix.fr>
2023-08-26 01:17:07 +0200

    handle errors at top level method

--- src/main/java/com/hubsante/model/edxl/UseCaseMessage.java
+++ src/main/java/com/hubsante/model/edxl/UseCaseMessage.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.hubsante.model.CustomMessage;
 import com.hubsante.model.cisu.*;
 import com.hubsante.model.emsi.Emsi;
+import com.hubsante.model.report.ErrorReport;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
 @JsonSubTypes({
         @JsonSubTypes.Type(CreateCaseMessage.class),
         @JsonSubTypes.Type(GenericAckMessage.class),
         @JsonSubTypes.Type(Emsi.class),
+        @JsonSubTypes.Type(ErrorReport.class),
         @JsonSubTypes.Type(CustomMessage.class)
 })
 public class UseCaseMessage {
---
+++ src/main/java/com/hubsante/model/report/ErrorReport.java
@@ -0,0 +1,60 @@
+package com.hubsante.model.report;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hubsante.model.edxl.UseCaseMessage;
+
+public class ErrorReport extends UseCaseMessage {
+
+    private final String JSON_PROPERTY_ERROR_CODE = "errorCode";
+    private ErrorCode errorCode;
+    private final String JSON_PROPERTY_ERROR_CAUSE = "errorCause";
+    private String errorCause;
+    private final String JSON_PROPERTY_SOURCE_MESSAGE = "sourceMessage";
+    private String sourceMessage;
+
+    public ErrorReport() {
+    }
+
+    public ErrorReport(ErrorCode errorCode, String errorCause, String sourceMessage) {
+        this.errorCode = errorCode;
+        this.errorCause = errorCause;
+        this.sourceMessage = sourceMessage;
+    }
+
+    @JsonProperty(JSON_PROPERTY_ERROR_CODE)
+    @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    @JsonProperty(JSON_PROPERTY_ERROR_CODE)
+    @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    @JsonProperty(JSON_PROPERTY_ERROR_CAUSE)
+    @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+    public String getErrorCause() {
+        return errorCause;
+    }
+
+    @JsonProperty(JSON_PROPERTY_ERROR_CAUSE)
+    @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+    public void setErrorCause(String errorCause) {
+        this.errorCause = errorCause;
+    }
+
+    @JsonProperty(JSON_PROPERTY_SOURCE_MESSAGE)
+    @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+    public String getSourceMessage() {
+        return sourceMessage;
+    }
+
+    @JsonProperty(JSON_PROPERTY_SOURCE_MESSAGE)
+    @JsonInclude(JsonInclude.Include.USE_DEFAULTS)
+    public void setSourceMessage(String sourceMessage) {
+        this.sourceMessage = sourceMessage;
+    }
+}
