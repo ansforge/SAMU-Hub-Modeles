3793c3654bbe856a5231cab0967794f2a7b6dbc2
bbo <bbonche@henix.fr>
2023-09-07 09:18:02 +0200

    ajout code erreurs enum

--- src/main/java/com/hubsante/model/report/ErrorCode.java
+++ src/main/java/com/hubsante/model/report/ErrorCode.java
@@ -1,11 +1,32 @@
 package com.hubsante.model.report;
 
 public enum ErrorCode {
-    NOT_ALLOWED_CONTENT_TYPE,
-    UNRECOGNIZED_MESSAGE_FORMAT,
-    INVALID_MESSAGE,
-    SENDER_INCONSISTENCY,
-    DELIVERY_MODE_INCONSISTENCY,
-    EXPIRED_MESSAGE_BEFORE_ROUTING,
-    DEAD_LETTER_QUEUED
+    NOT_ALLOWED_CONTENT_TYPE(101, "NOT_ALLOWED_CONTENT_TYPE"),
+    UNRECOGNIZED_MESSAGE_FORMAT(102, "UNRECOGNIZED_MESSAGE_FORMAT"),
+    DELIVERY_MODE_INCONSISTENCY(100, "DELIVERY_MODE_INCONSISTENCY"),
+    SENDER_INCONSISTENCY(200, "SENDER_INCONSISTENCY"),
+    INVALID_MESSAGE(300, "INVALID_MESSAGE"),
+    EXPIRED_MESSAGE_BEFORE_ROUTING(400, "EXPIRED_MESSAGE_BEFORE_ROUTING"),
+    DEAD_LETTER_QUEUED(500, "DEAD_LETTER_QUEUED");
+
+    ErrorCode(int statusCode, String statusString) {
+    }
+
+    private int statusCode;
+
+    public boolean isSetupError() {
+        return this.statusCode <200;
+    }
+
+    public boolean isUnauthorizedError() {
+        return this.statusCode >= 200 && this.statusCode < 300;
+    }
+
+    public boolean isParsingError() {
+        return this.statusCode >= 300 && this.statusCode < 400;
+    }
+
+    public boolean isExpired() {
+        return this.statusCode >= 400 && this.statusCode < 500;
+    }
 }
