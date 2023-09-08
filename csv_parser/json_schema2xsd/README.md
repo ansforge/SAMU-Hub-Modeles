34ba83754f39fe144b7b9f41f03ce48aef175a85
Romain Fouilland <romain.fd@gmail.com>
2023-09-08 15:06:31 +0200

    Adding JSON Schema to XSD converter

---
+++ csv_parser/json_schema2xsd/README.md
@@ -0,0 +1,7 @@
+# JSON Schema to XSD converter
+Code based on [jsons2xsd](https://github.com/ethlo/jsons2xsd) to convert our JSON Schema specs to XSD.
+
+## Usage
+```bash
+gradle run
+```
---
+++ csv_parser/json_schema2xsd/src/main/java/json_schema2xsd/App.java
@@ -0,0 +1,74 @@
+package json_schema2xsd;
+
+import com.ethlo.jsons2xsd.Config;
+import com.ethlo.jsons2xsd.Jsons2Xsd;
+import org.w3c.dom.Document;
+
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.dom.DOMSource;
+import java.io.InputStream;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+
+        // Specify the path to your JSON schema file
+        String jsonSchemaResourcePath = "/schema.json";
+
+        // Specify the path to save the generated XSD file
+        String xsdFilePath = "out" + jsonSchemaResourcePath.replace(".json", ".xsd");
+
+        // Create a Config object to customize the conversion
+        Config config = new Config.Builder()
+                .targetNamespace("https://hub.esante.gouv.fr/createCase.xsd")
+                .name("createCase")
+                .build();
+
+        try {
+            // Read the JSON schema from the classpath resources
+            InputStream jsonSchemaStream = App.class.getResourceAsStream(jsonSchemaResourcePath);
+
+            // Create a Reader from the InputStream
+            InputStreamReader reader = new InputStreamReader(jsonSchemaStream);
+
+            // Convert the JSON schema to an XML schema
+            Document xmlSchemaDoc = Jsons2Xsd.convert(reader, config);
+
+            // Convert the Document to a String
+            String xmlSchema = documentToString(xmlSchemaDoc);
+
+            // Write the generated XML schema to a file
+            writeDocumentToFile(xmlSchemaDoc, xsdFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Helper method to convert a DOM Document to a String
+    private static String documentToString(Document document) throws Exception {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(document), new StreamResult(writer));
+        return writer.getBuffer().toString();
+    }
+
+    // Helper method to write a DOM Document to a file
+    private static void writeDocumentToFile(Document document, String filePath) throws Exception {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        FileOutputStream outputStream = new FileOutputStream(filePath);
+        transformer.transform(new DOMSource(document), new StreamResult(outputStream));
+        outputStream.close();
+    }
+}
+
