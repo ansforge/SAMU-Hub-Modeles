945a117cb5ea3d1220d8f518e2ee0d011bb35285
bbonche <bbonche@henix.fr>
2023-10-12 12:06:00 +0200

    migrate test messages

--- src/main/java/com/hubsante/model/CustomMessage.java
+++ src/main/java/com/hubsante/model/CustomMessage.java
@@ -5,7 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.hubsante.model.cisu.CreateCaseMessage;
 import com.hubsante.model.edxl.ContentMessage;
+
+import java.util.Objects;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @JacksonXmlRootElement(localName = "message")
@@ -26,5 +29,22 @@ public class CustomMessage extends ContentMessage {
     @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
     public void setCustomContent(JsonNode customContent) {
         this.customContent = customContent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CustomMessage customMessage = (CustomMessage) o;
+        return Objects.equals(this.customContent, customMessage.customContent);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customContent);
     }
 }
