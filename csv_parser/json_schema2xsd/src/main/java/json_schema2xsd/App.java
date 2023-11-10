2043f0762106bbc6471698ccbd5f4ca3607c2fe8
Romain Fouilland <romain.fd@gmail.com>
2023-11-10 18:21:40 +0100

    Mise à jour pour projectathon

--- csv_parser/json_schema2xsd/src/main/java/json_schema2xsd/App.java
+++ csv_parser/json_schema2xsd/src/main/java/json_schema2xsd/App.java
@@ -22,7 +22,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
 
-        for (String schema : Arrays.asList("EDXL-DE", "RC-DE", "RC-EDA")) {
+        for (String schema : Arrays.asList("RC-EDA", "EMSI-DC")) {
             // Specify the path to your JSON schema file
             String jsonSchemaResourcePath = "/" + schema + "_schema.json";
 
