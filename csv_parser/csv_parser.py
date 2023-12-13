58c14c45992ebabb55edd29cfa212c4cd1741632
Romain Fouilland <romain.fd@gmail.com>
2023-12-13 22:38:42 +0100

    Try at handling already defined object in example building

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -130,10 +130,14 @@ df.loc[df['Nouvelle balise'].notnull(), 'name'] = df['Nouvelle balise']
 # Get level in data hierarchy
 df['level_shift'] = -1
 for i in range(1, 1 + DATA_DEPTH):
+    # Number of distinct values in 'level i' column before that item included
     df[f"level_{i}"] = df[f"Donnée (Niveau {i})"].notnull().cumsum()
+    # Number of distinct values in 'level i' column before that item excluded
     df[f"previous_level_{i}"] = df[f"level_{i}"].shift(periods=1, fill_value=0)
+    # If the 2 numbers are different => that item's active 'Donnée' column is i => update 'level_shift' column
     df["level_shift"] = df.apply(
         lambda row: i if row[f"level_{i}"] != row[f"previous_level_{i}"] else row['level_shift'], axis=1)
+
 
 # DATA VALIDATION
 HAS_ERROR = False
@@ -275,6 +279,21 @@ def is_array(elem):
     return elem['Cardinalité'].endswith('n')
 
 
+def navigate_children_with_id(id_path):
+    """
+    Helper to navigate through children tree using item id.
+    ex : '1.1.11' gives children['1'][0]['children'][11]['children']
+    """
+    # Removing wrapper
+    current = children['1']
+    id_path = id_path[2:]
+    # Navigating following given id_path
+    keys = list(map(int, id_path.split('.')))
+    for key in keys:
+        current = current[key - 1]['children']
+    return current
+
+
 # Recursively build a json example
 def build_example(elem):
     if elem['Objet'] != 'X':
@@ -282,6 +301,11 @@ def build_example(elem):
             return 'None'
         return elem['Exemples']
     elif 'children' not in elem:
+        # ToDo: Could be object reference (so no children itself but first definition of its type has)
+        # But it is not so easy to access the first reference here as nor the JSON Schema nor example are built yet
+        # .id is not correct as the ID don't reset but are always increasing... computing some resetting id is necessary
+        # type_first_definition = df[df['true_type'] == elem['true_type']].iloc[0].id
+        # type_children = navigate_children_with_id(type_first_definition)
         return {}
     else:
         children = {}
@@ -448,7 +472,12 @@ def get_examples_with_json_example(definition):
     json_schema_path = "$" + definition['example'].split('#')[1]
     path_with_arrays = json_schema_path.replace('/0', '[0]')
     path = path_with_arrays.replace('/', '.')
-    example = parse(path).find(json_example)[0].value
+    try:
+        example = parse(path).find(json_example)[0].value
+    except Exception as err:
+        print(f"{Color.ORANGE}WARNING: '{err}' while getting example from JSON sample.{Color.ORANGE}")
+        print(f"Path {path} of data '{definition['title']}' not found. Make sure parents and types are OK.{Color.END}")
+        return
     if depth(example) < 2:
         definition['examples'] = [example]
     # Do it as well for all its children properties
