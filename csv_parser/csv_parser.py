6e01d3d10d959937795cb2a935088e757f7dd65a
bbo <bbonche@henix.fr>
2024-01-11 09:31:41 +0100

    resolve conflicts

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -338,7 +338,7 @@ def run(sheet, name, version, filter):
         'title': MODEL_NAME,
         'required': [],
         'properties': {},
-        'additionalProperties': false,
+        'additionalProperties': False,
         'definitions': {}
     }
 
