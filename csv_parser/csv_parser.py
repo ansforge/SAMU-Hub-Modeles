8c14fd307cf6af2569daae67b71c4301c4b1f309
Romain Fouilland <romain.fd@gmail.com>
2024-02-13 18:04:00 +0100

    chore/csv_parser: cleaning OpenAPI / AsyncAPI

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -123,25 +123,7 @@ def run(sheet, name, version, filter):
     # Replacing comment cells (starting with '# ') with NaN in 'Donnée xx' columns
     df.iloc[:, 1:1 + DATA_DEPTH] = df.iloc[:, 1:1 + DATA_DEPTH].applymap(
         lambda x: pd.NA if str(x).startswith('# ') else x)
-    # if MODEL_NAME != "RC-DE":
-    #     # Adding the wrapper
-    #     # - Moving all levels one level down
-    #     df = df.rename(columns={f"Donnée (Niveau {i})": f"Donnée (Niveau {i + 1})" for i in range(1, DATA_DEPTH + 1)})
-    #     DATA_DEPTH += 1
-    #     # - Adding the wrapper line
-    #     df.insert(1, "Donnée (Niveau 1)", None)
-    #     df = pd.concat([
-    #         pd.DataFrame({
-    #             'ID': -1,
-    #             'Description': f"Object {MODEL_NAME}",
-    #             'Nouvelle balise': MODEL_TYPE,
-    #             'Cardinalité': '1..1',
-    #             'Objet': 'X',
-    #             'Format (ou type)': MODEL_TYPE,
-    #             'Donnée (Niveau 1)': f"Objet {MODEL_NAME}"
-    #         }, index=[-1]),
-    #         df
-    #     ])
+
     # Adding a name column (NexSIS by default, overriden by 'Nouvelle Balise' if exists)
     df['name'] = df['Balise NexSIS']
     df.loc[df['Nouvelle balise'].notnull(), 'name'] = df['Nouvelle balise']
@@ -528,9 +510,10 @@ def run(sheet, name, version, filter):
         json.dump(json_schema, outfile, indent=4)
     print('JSON schema generated.')
 
-    # BUILD AsyncAPI SCHEMA
-    def build_asyncapi_schema():
-        asyncapi_schema = {}
+    # BUILD OpenAPI SCHEMA
+    print(f'{Color.BOLD}{Color.UNDERLINE}{Color.PURPLE}Generating OpenAPI schema...{Color.END}')
+    def json_schema_to_openapi_schema(json_schema):
+        openapi_schema = {}
         definitions = json_schema['definitions']
         # Add root object to definitions so it is treated as a normal object
         # Dropping useless root infos
@@ -544,14 +527,13 @@ def run(sheet, name, version, filter):
         # Simply collect all objects (and root properties)
         for elem_name, definition in definitions.items():
             get_examples_with_json_example(definition)
-            asyncapi_schema[elem_name] = definition
-        return asyncapi_schema
+            openapi_schema[elem_name] = definition
+        return openapi_schema
 
-    openapi_components = build_asyncapi_schema()
+    openapi_components = json_schema_to_openapi_schema(json_schema)
     with open('common.openapi.yaml') as f:
         common_openapi_components = yaml.load(f, Loader=yaml.loader.SafeLoader)
 
-    print(f'{Color.BOLD}{Color.UNDERLINE}{Color.PURPLE}Generating OpenAPI schema...{Color.END}')
     with open('template.openapi.yaml') as f:
         full_yaml = yaml.load(f, Loader=yaml.loader.SafeLoader)
 
@@ -573,7 +555,13 @@ def run(sheet, name, version, filter):
             **openapi_components
         }
 
-    print(f'{Color.BOLD}{Color.UNDERLINE}{Color.PURPLE}Adding schema info to AsyncAPI spec...{Color.END}')
+    with open(f'out/{name}/{name}.openapi.yaml', 'w') as file:
+        documents = yaml.dump(full_yaml, sort_keys=False)
+        documents = documents.replace('#/definitions/', "#/components/schemas/")
+        file.write(documents)
+    print('OpenAPI schema generated.')
+
+    print(f'{Color.BOLD}{Color.UNDERLINE}{Color.PURPLE}Collecting AsyncAPI spec...{Color.END}')
     with open('template.asyncapi.yaml') as f:
         asyncapi_yaml = yaml.load(f, Loader=yaml.loader.SafeLoader)
 
@@ -589,12 +577,7 @@ def run(sheet, name, version, filter):
             full_asyncapi = asyncapi_yaml
         else:
             full_asyncapi['components']['schemas'].update(asyncapi_yaml['components']['schemas'])
-
-    with open(f'out/{name}/{name}.openapi.yaml', 'w') as file:
-        documents = yaml.dump(full_yaml, sort_keys=False)
-        documents = documents.replace('#/definitions/', "#/components/schemas/")
-        file.write(documents)
-    print('OpenAPI schema generated.')
+    print('AsyncAPI schema collected.')
 
     print(f'{Color.BOLD}{Color.UNDERLINE}{Color.PURPLE}Generating UML diagrams...{Color.END}')
     uml_generator.run(name, MODEL_NAME, version=version, filter=filter)
