061ba9a0dbb8236c84f0f1b3a2676054e388ec90
bbo <bbonche@henix.fr>
2024-03-18 19:12:07 +0100

    polish

--- src/main/java/com/hubsante/model/custom/CustomMessage.java
+++ src/main/java/com/hubsante/model/custom/CustomMessage.java
@@ -18,7 +18,6 @@ package com.hubsante.model.custom;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.hubsante.model.edxl.ContentMessage;
