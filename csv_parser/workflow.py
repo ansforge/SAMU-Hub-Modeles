08216231ccacd7e832e94e37863d658ba4e22c27
ssviridov.henix <ssviridov@henix.fr>
2024-06-12 14:53:31 +0200

    feat/parser: fix comment

--- csv_parser/workflow.py
+++ csv_parser/workflow.py
@@ -63,7 +63,7 @@ def parser_and_mv():
                       f"It was: '{full_df.iloc[1, 0]}'")
                 exit(1)
         else:
-            print(f"Error in sheet {sheet}: schema list (cell A2) is empty. "
+            print(f"Error in sheet {sheet}: schema list (cell A1) is empty. "
                   f"Should be 'name:filter:modelType' separated by a space. "
                   f"Ex: 'RC-EDA:15-18:createCase RS-EDA:15-15:createCaseHealth'.")
             exit(1)
