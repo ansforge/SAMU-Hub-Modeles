10732c7e0a5ea035651231463671fcdcd83cd81b
saveliy-sviridov <saveliy-sviridov@users.noreply.github.com>
2024-06-20 13:32:26 +0000

    ⚙️ Auto-génération des classes et des specs

---
+++ com/hubsante/model/TempMain.java
@@ -0,0 +1,10 @@
+
+package com.hubsante.model;
+
+public class TempMain {
+  public static void main(String[] args) throws Exception {
+    XmlGenerationHelper xmlGenerationHelper = new XmlGenerationHelper();
+    xmlGenerationHelper.generateXmlFiles();
+  }       
+}
+
