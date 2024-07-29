bbd4ee823fb9c7071ee242fd4c2ca942a7668878
bbo <bbonche@henix.fr>
2024-07-29 22:16:57 +0200

    add main class

---
+++ src/main/java/com/hubsante/model/XmlGenerator.java
@@ -0,0 +1,9 @@
+package com.hubsante.model;
+
+public class XmlGenerator {
+
+    public static void main(String[] args) {
+        XmlGenerationHelper xmlGenerationHelper = new XmlGenerationHelper();
+        xmlGenerationHelper.generateXmlFiles();
+    }
+}
