28975bee432c923425618828cc8f643b95098271
ssviridov <sav.sviridov@henix.fr>
2024-11-27 16:06:00 +0100

    feat/auto: add templates for schemas and update the script

--- automatic-schema-generator/automatic-generator.sh
+++ automatic-schema-generator/automatic-generator.sh
@@ -1,11 +1,20 @@
 # We will be using the file schemas.yaml as data source to use with gomplate in order to generate all the files
 # required for each schema
 
-# Run the script to generate base generator config files
+# generate base generator config files
 gomplate -f ./templates/schema.generator-config.json.tmpl -d config=./schemas.yaml -o ./output
 
-# Run the script to generate usecase config files
+# generate usecase config files
 gomplate -f ./templates/schema.usecase.generator-config.json.tmpl -d config=./schemas.yaml -o ./output
 
-# Run the script to generate wrapper config files
+# generate wrapper config files
 gomplate -f ./templates/schema.wrapper.generator-config.json.tmpl -d config=./schemas.yaml -o ./output
+
+# generate ContentMessage class
+gomplate -f ./templates/ContentMessage.java.tmpl -d config=./schemas.yaml -o ./output/ContentMessage.java
+
+# generate EDXL-DE json schema
+gomplate -f ./templates/EDXL-DE-full.schema.json.tmpl -d config=./schemas.yaml -o ./output/EDXL-DE-full.schema.json
+
+# generate RC-XML-ContentType xsd schema
+gomplate -f ./templates/RC-XML-ContentType.xsd.tmpl -d config=./schemas.yaml -o ./output/RC-XML-ContentType.xsd
