81d37f44c6e07101bf41943d7514ab960b437d89
ssviridov <sav.sviridov@henix.fr>
2024-12-02 18:19:28 +0100

    feat/auto: update templates and example files

--- src/main/java/com/hubsante/model/report/Error.java
+++ src/main/java/com/hubsante/model/report/Error.java
@@ -52,7 +52,7 @@ import java.util.Objects;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 
 public class Error {
-  @JacksonXmlProperty(isAttribute = true) String xmlns = "urn:emergency:";
+  @JacksonXmlProperty(isAttribute = true) String xmlns = "urn:emergency:cisu:2.0:error";
   public static final String JSON_PROPERTY_ERROR_CODE = "errorCode";
   private ErrorCode errorCode;
 
