b4779e73fee7c3ffa81b57da4d37faba8a0a2f91
saveliy-sviridov <saveliy-sviridov@users.noreply.github.com>
2024-12-12 14:20:20 +0000

    ⚙️ Auto-génération des classes et des specs

--- src/main/java/com/hubsante/model/edxl/ContentMessage.java
+++ src/main/java/com/hubsante/model/edxl/ContentMessage.java
@@ -17,45 +17,49 @@ package com.hubsante.model.edxl;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.hubsante.model.cisu.CreateCase;
-import com.hubsante.model.cisu.CreateCaseWrapper;
-import com.hubsante.model.custom.CustomMessage;
-import com.hubsante.model.documentlink.DocumentLink;
-import com.hubsante.model.emsi.Emsi;
-import com.hubsante.model.emsi.EmsiWrapper;
-import com.hubsante.model.geolocation.*;
-import com.hubsante.model.health.CreateCaseHealth;
-import com.hubsante.model.health.CreateCaseHealthUpdate;
-import com.hubsante.model.health.CreateCaseHealthUpdateWrapper;
-import com.hubsante.model.health.CreateCaseHealthWrapper;
-import com.hubsante.model.interventionreport.InterventionReport;
-import com.hubsante.model.reference.Reference;
-import com.hubsante.model.reference.ReferenceWrapper;
-import com.hubsante.model.report.ErrorWrapper;
-import com.hubsante.model.resources.info.ResourcesEngagement;
-import com.hubsante.model.resources.info.ResourcesInfo;
-import com.hubsante.model.resources.info.ResourcesInfoWrapper;
-import com.hubsante.model.resources.request.ResourcesRequest;
-import com.hubsante.model.resources.request.ResourcesRequestWrapper;
-import com.hubsante.model.resources.response.ResourcesResponse;
-import com.hubsante.model.resources.response.ResourcesResponseWrapper;
-import com.hubsante.model.resources.status.ResourcesStatus;
-import com.hubsante.model.resources.status.ResourcesStatusWrapper;
-import com.hubsante.model.rpis.Rpis;
-import com.hubsante.model.rpis.RpisWrapper;
+
 import com.hubsante.model.technical.Technical;
 import com.hubsante.model.technical.TechnicalWrapper;
 import com.hubsante.model.technical.noreq.TechnicalNoreq;
 import com.hubsante.model.technical.noreq.TechnicalNoreqWrapper;
-import com.hubsante.model.documentlink.DocumentLinkWrapper;
+import com.hubsante.model.cisu.CreateCase;
+import com.hubsante.model.cisu.CreateCaseWrapper;
+import com.hubsante.model.health.CreateCaseHealth;
+import com.hubsante.model.health.CreateCaseHealthWrapper;
+import com.hubsante.model.health.CreateCaseHealthUpdate;
+import com.hubsante.model.health.CreateCaseHealthUpdateWrapper;
+import com.hubsante.model.emsi.Emsi;
+import com.hubsante.model.emsi.EmsiWrapper;
+import com.hubsante.model.resources.info.ResourcesInfo;
+import com.hubsante.model.resources.info.ResourcesInfoWrapper;
+import com.hubsante.model.resources.info.ResourcesEngagement;
 import com.hubsante.model.resources.info.ResourcesEngagementWrapper;
+import com.hubsante.model.resources.status.ResourcesStatus;
+import com.hubsante.model.resources.status.ResourcesStatusWrapper;
+import com.hubsante.model.resources.request.ResourcesRequest;
+import com.hubsante.model.resources.request.ResourcesRequestWrapper;
+import com.hubsante.model.resources.response.ResourcesResponse;
+import com.hubsante.model.resources.response.ResourcesResponseWrapper;
+import com.hubsante.model.rpis.Rpis;
+import com.hubsante.model.rpis.RpisWrapper;
+import com.hubsante.model.interventionreport.InterventionReport;
 import com.hubsante.model.interventionreport.InterventionReportWrapper;
-
-
+import com.hubsante.model.documentlink.DocumentLink;
+import com.hubsante.model.documentlink.DocumentLinkWrapper;
+import com.hubsante.model.geolocation.GeoPositionsUpdate;
+import com.hubsante.model.geolocation.GeoPositionsUpdateWrapper;
+import com.hubsante.model.geolocation.GeoResourcesRequest;
+import com.hubsante.model.geolocation.GeoResourcesRequestWrapper;
+import com.hubsante.model.geolocation.GeoResourcesDetails;
+import com.hubsante.model.geolocation.GeoResourcesDetailsWrapper;
+import com.hubsante.model.reference.Reference;
+import com.hubsante.model.reference.ReferenceWrapper;
+import com.hubsante.model.report.ErrorWrapper;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
 @JsonSubTypes({ 
         @JsonSubTypes.Type(TechnicalWrapper.class),
         @JsonSubTypes.Type(TechnicalNoreqWrapper.class),
