6fcce0c22711818ab75982def65a1fa4d888e667
Romain Fouilland <romain.fd@gmail.com>
2024-12-24 11:02:53 +0100

    feat: adding pypi package

--- automatic-schema-generator/automatic-generator.sh
+++ automatic-schema-generator/automatic-generator.sh
@@ -1,23 +1,23 @@
 # We will be using the file schemas.yaml as data source to use with gomplate in order to generate all the files
 # required for each schema
 
-# generate base generator config files
-gomplate -f ./templates/schema.generator-config.json.tmpl -d config=./schemas.yaml
+# Generate config files for each language
+for lang in "." "ruby" "python"; do
+    # Handle base path for non-root language
+    template_path="./templates"
+    if [ "$lang" != "." ]; then
+        template_path="$template_path/$lang"
+    fi
 
-# generate usecase config files
-gomplate -f ./templates/schema.usecase.generator-config.json.tmpl -d config=./schemas.yaml
+    # generate base generator config files
+    gomplate -f "./templates/$lang/schema.generator-config.json.tmpl" -d config=./schemas.yaml
 
-# generate wrapper config files
-gomplate -f ./templates/schema.wrapper.generator-config.json.tmpl -d config=./schemas.yaml
+    # generate usecase config files  
+    gomplate -f "./templates/$lang/schema.usecase.generator-config.json.tmpl" -d config=./schemas.yaml
 
-# generate ruby base generator config files
-gomplate -f ./templates/ruby/schema.generator-config.json.tmpl -d config=./schemas.yaml
-
-# generate ruby usecase config files
-gomplate -f ./templates/ruby/schema.usecase.generator-config.json.tmpl -d config=./schemas.yaml
-
-# generate ruby wrapper config files
-gomplate -f ./templates/ruby/schema.wrapper.generator-config.json.tmpl -d config=./schemas.yaml
+    # generate wrapper config files
+    gomplate -f "./templates/$lang/schema.wrapper.generator-config.json.tmpl" -d config=./schemas.yaml
+done
 
 # generate ContentMessage class
 gomplate -f ./templates/ContentMessage.java.tmpl -d config=./schemas.yaml -o ./output/edxl/ContentMessage.java
---
+++ generator_python/README.md
@@ -0,0 +1,2 @@
+# Hub Santé Modèles
+Python library for the [Hub Santé](https://hub.esante.gouv.fr)
