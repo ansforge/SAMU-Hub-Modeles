fdfdeefe2a057f4b28c873ed8c9f98cf1810f375
romainfd <romainfd@users.noreply.github.com>
2024-12-31 08:33:17 +0000

    ⚙️ Auto-génération des librairies

--- generator_python/package/src/hubsante_model/health/models/risk_threat.py
+++ generator_python/package/src/hubsante_model/health/models/risk_threat.py
@@ -19,6 +19,7 @@ import json
 
 from pydantic import BaseModel, ConfigDict, Field, StrictStr, field_validator
 from typing import Any, ClassVar, Dict, List
+from typing_extensions import Annotated
 from typing import Optional, Set
 from typing_extensions import Self
 
@@ -26,15 +27,15 @@ class RiskThreat(BaseModel):
     """
     RiskThreat
     """ # noqa: E501
-    code: StrictStr = Field(description="A valoriser avec le code de la nomenclature associée")
+    code: Annotated[str, Field(strict=True)] = Field(description="A valoriser avec le code de la nomenclature associée")
     label: StrictStr = Field(description="A valoriser avec le libellé de la nomenclature associée. Dans le cas où un système n'est pas en mesure de reconnaître un code, il peut choisir d'afficher le libellé qui est obligatoirement fourni avec le code.")
     __properties: ClassVar[List[str]] = ["code", "label"]
 
     @field_validator('code')
-    def code_validate_enum(cls, value):
-        """Validates the enum"""
-        if value not in set(['R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 'R08', 'R09', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'R16', 'R17', 'R18', 'R19', 'R20', 'R21', 'R22', 'R23', 'R24', 'R25', 'R26', 'R27', 'R28', 'R29', 'R30', 'R31', 'R32', 'R33', 'R34', 'R35', 'R36', 'R37']):
-            raise ValueError("must be one of enum values ('R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 'R08', 'R09', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'R16', 'R17', 'R18', 'R19', 'R20', 'R21', 'R22', 'R23', 'R24', 'R25', 'R26', 'R27', 'R28', 'R29', 'R30', 'R31', 'R32', 'R33', 'R34', 'R35', 'R36', 'R37')")
+    def code_validate_regular_expression(cls, value):
+        """Validates the regular expression"""
+        if not re.match(r"^R\d{2}$", value):
+            raise ValueError(r"must validate the regular expression /^R\d{2}$/")
         return value
 
     model_config = ConfigDict(
