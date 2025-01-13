e1aa55a005d8156966cf20ec2f03d7f34c29d71a
bou3108 <68332716+bou3108@users.noreply.github.com>
2025-01-13 08:18:40 +0000

    ⚙️ Auto-génération des classes et des specs

--- src/main/java/com/hubsante/model/interventionreport/InterventionReport.java
+++ src/main/java/com/hubsante/model/interventionreport/InterventionReport.java
@@ -56,7 +56,7 @@ import java.util.Objects;
 
 public class InterventionReport {
   @JacksonXmlProperty(isAttribute = true)
-  String xmlns = "urn:emergency:eda:1.9:interventionreport";
+  String xmlns = "urn:emergency:cisu:3.0:interventionreport";
   public static final String JSON_PROPERTY_CASE_ID = "caseId";
   private String caseId;
 
