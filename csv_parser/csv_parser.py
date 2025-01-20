f104bdcf53da1d280c5482fbccc63440b7076a79
Romain Fouilland <romain.fd@gmail.com>
2025-01-20 14:48:51 +0100

    feat: reverting changes to trigger action

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -12,15 +12,6 @@ import warnings
 import uml_generator
 import os
 
-
-
-
-
-
-
-
-
-
 from pathlib import Path
 # Improving panda printing | Ref.: https://stackoverflow.com/a/11711637
 pd.set_option('display.max_rows', 500)
