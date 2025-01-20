f2c342bc04af97e2d994565e071f4f7892c78efb
Romain Fouilland <romain.fd@gmail.com>
2025-01-20 13:07:01 +0100

    feat: triggering github action

--- csv_parser/csv_parser.py
+++ csv_parser/csv_parser.py
@@ -12,6 +12,7 @@ import warnings
 import uml_generator
 import os
 
+
 from pathlib import Path
 # Improving panda printing | Ref.: https://stackoverflow.com/a/11711637
 pd.set_option('display.max_rows', 500)
