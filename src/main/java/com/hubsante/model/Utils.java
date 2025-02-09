9c9f5e79a89f1603b0ad546afe46487516d53c5e
bbo <benjamin.bonche@gmail.com>
2025-02-09 18:06:20 +0100

    backport custom date serializer

---
+++ src/main/java/com/hubsante/model/Utils.java
@@ -0,0 +1,60 @@
+package com.hubsante.model;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Utils {
+
+    public static JavaTimeModule createCustomJavaTimeModule() {
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+        javaTimeModule.addSerializer(OffsetDateTime.class,
+                new com.fasterxml.jackson.databind.ser.std.StdSerializer<OffsetDateTime>(OffsetDateTime.class) {
+
+                    @Override
+                    public void serialize(OffsetDateTime value, com.fasterxml.jackson.core.JsonGenerator gen,
+                                          com.fasterxml.jackson.databind.SerializerProvider provider) throws IOException {
+                        String formatted = value.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"));
+                        // Replace 'Z' with '+00:00' during serialization
+                        if (formatted.endsWith("Z")) {
+                            formatted = formatted.substring(0, formatted.length() - 1) + "+00:00";
+                        }
+
+                        gen.writeString(formatted);
+                    }
+                }
+        );
+
+        return javaTimeModule;
+    }
+
+    public static XmlMapper getXmlMapper() {
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper = (XmlMapper) new XmlMapper()
+                .registerModule(createCustomJavaTimeModule())
+                .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+
+        xmlMapper.configure(com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true);
+        return xmlMapper;
+    }
+
+    public static ObjectMapper getJsonMapper() {
+        ObjectMapper jsonMapper = new ObjectMapper()
+                .registerModule(createCustomJavaTimeModule())
+                .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+
+        return jsonMapper;
+    }
+}
