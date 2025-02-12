e32377dbafba99696a4643e16e3cb5a3258f79e7
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-02-12 14:36:43 +0100

    feat(client java): add snapshot tests on converter

--- converter/tests/test_cisu_converter.py
+++ converter/tests/test_cisu_converter.py
@@ -1,5 +1,12 @@
+import unittest
+from unittest.mock import patch
 from converter.cisu_converter import CISUConverterV3
 from .test_helpers import TestHelper
+import json
+from snapshottest import TestCase
+from datetime import datetime
+import pytest
+import random
 
 # Load schemas once for all tests
 EDXL_FULL_SCHEMA = TestHelper.load_schema("EDXL-DE-full.schema.json")
@@ -51,3 +58,58 @@ def test_to_cisu_conversion_v3():
         target_schema=RC_EDA_SCHEMA,
         online_tag="main"  # ToDo: migrate to "v3" once tag is available
     )
+
+
+class TestSnapshotCisuConverter(TestCase):
+    def setUp(self):
+        self.edxl_envelope_health_to_fire_path = "tests/edxl_envelope_health_to_fire.json"
+        self.edxl_envelope_fire_to_health_path = "tests/edxl_envelope_fire_to_health.json"
+        self.fixtures_folder_path = "tests/fixtures/"
+
+    @patch('converter.cisu_converter.datetime')
+    @patch('converter.cisu_converter.random')
+    def test_snapshot_RS_EDA_exhaustive_message(self, mock_choices, mock_now):
+        mock_now.now.return_value = datetime(2024, 2, 10, 12, 34, 56)
+        mock_now.strftime = datetime.strftime
+
+        mock_choices.choices.return_value = "f5de"
+
+        message = TestHelper.create_edxl_json_from_sample(self.edxl_envelope_health_to_fire_path, self.fixtures_folder_path + "RS-EDA_exhaustive_fill.json")
+        converter = CISUConverterV3()
+
+        output_data = converter.to_cisu(message)
+        self.assertMatchSnapshot(json.dumps(output_data, indent=2))
+
+    def test_snapshot_RC_EDA_exhaustive_message(self):
+        message = TestHelper.create_edxl_json_from_sample(self.edxl_envelope_fire_to_health_path, self.fixtures_folder_path + "RC-EDA_exhaustive_fill.json")
+        converter = CISUConverterV3()
+
+        output_data = converter.from_cisu(message)
+        print(output_data['content'][0]['jsonContent']['embeddedJsonContent']['message']['createCaseHealth']['initialAlert']['notes'])
+        self.assertMatchSnapshot(json.dumps(output_data, indent=2))
+
+    @patch("converter.cisu_converter.datetime")
+    @patch('converter.cisu_converter.random')
+    def test_snapshot_RS_EDA_required_field_message(self,mock_choices, mock_now):
+        mock_now.now.return_value = datetime(2024, 2, 10, 12, 34, 56)
+        mock_now.strftime = datetime.strftime
+
+        mock_choices.choices.return_value = "f5de"
+
+        message = TestHelper.create_edxl_json_from_sample(self.edxl_envelope_health_to_fire_path, self.fixtures_folder_path + "RS-EDA_required_fields.json")
+        converter = CISUConverterV3()
+
+        output_data = converter.to_cisu(message)
+        self.assertMatchSnapshot(json.dumps(output_data, indent=2))
+
+    def test_snapshot_RC_EDA_required_field_message(self):
+        message = TestHelper.create_edxl_json_from_sample(self.edxl_envelope_fire_to_health_path, self.fixtures_folder_path + "RC-EDA_required_fields.json")
+        converter = CISUConverterV3()
+
+        output_data = converter.from_cisu(message)
+
+        self.assertMatchSnapshot(json.dumps(output_data, indent=2))
+
+
+if __name__ == "__main__":
+    unittest.main()
