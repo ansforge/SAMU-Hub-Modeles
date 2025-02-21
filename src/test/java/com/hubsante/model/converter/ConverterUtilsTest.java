6b27c423d3eda3c4cb20cb91379a5bd8eae4d22e
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-02-21 17:49:42 +0100

    feat(test): use sample examples files to test is cisu

--- src/test/java/com/hubsante/model/converter/ConverterUtilsTest.java
+++ src/test/java/com/hubsante/model/converter/ConverterUtilsTest.java
@@ -6,42 +6,56 @@ import com.hubsante.model.edxl.EdxlMessage;
 
 import org.junit.jupiter.api.Test;
 
+import static com.hubsante.model.EdxlWrapperUtils.wrapUseCaseMessage;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class ConverterUtilsTest {
-    @Test
-    public void testIsCisuModel() throws IOException {
+    public void isCisuModelUtil(String resourcePath, boolean isCisu) {
         EdxlHandler converter = new EdxlHandler();
 
-        File RCJsonMessage = new File(Objects.requireNonNull(TestMessagesHelper.class.getClassLoader().getResource("sample/valid/RC-EDA/RC-EDA.json")).getFile());
-        String RCJsonUseCase = new String(Files.readAllBytes(RCJsonMessage.toPath()), StandardCharsets.UTF_8);
-        EdxlMessage RCMessage = converter.deserializeJsonEDXL(RCJsonUseCase);
-        assertTrue(ConverterUtils.isCisuModel(RCMessage));
+        String rootFolder = TestMessagesHelper.class.getClassLoader().getResource(resourcePath).getFile();
+        File[] subFiles = new File(rootFolder).listFiles(File::isDirectory);
+        List<File> exampleFiles = new ArrayList<>();
 
-        File RSJsonMessage = new File(Objects.requireNonNull(TestMessagesHelper.class.getClassLoader().getResource("sample/valid/RS-EDA/RS-EDA.json")).getFile());
-        String RSUseCaseJson = new String(Files.readAllBytes(RSJsonMessage.toPath()), StandardCharsets.UTF_8);
-        EdxlMessage RSMessage = converter.deserializeJsonEDXL(RSUseCaseJson);
-        assertTrue(ConverterUtils.isCisuModel(RSMessage));
+        assert subFiles != null;
+        Arrays.stream(subFiles).forEach(file -> {
+            exampleFiles.addAll(Arrays.asList(Objects.requireNonNull(file.listFiles())));
+        });
+
+        exampleFiles.forEach(file -> {
+            try {
+                if (file.getName().endsWith(".json")) {
+                    String jsonUseCase = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+                    String fullMessage = wrapUseCaseMessage(jsonUseCase);
+                    EdxlMessage message = converter.deserializeJsonEDXL(fullMessage);
+
+                    assertEquals(ConverterUtils.isCisuModel(message), isCisu);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Test
+    public void testIsCisuModel() throws IOException {
+        isCisuModelUtil("sample/examples/RS-EDA", true);
+        isCisuModelUtil("sample/examples/RC-EDA", true);
     }
 
     @Test
     public void testIsNotCisuModel() throws IOException {
-        EdxlHandler converter = new EdxlHandler();
-
-        File EDXLDEJsonMessage = new File(Objects.requireNonNull(TestMessagesHelper.class.getClassLoader().getResource("sample/valid/EDXL-DE/EDXL-DE.json")).getFile());
-        String EDXLDEJsonUseCase = new String(Files.readAllBytes(EDXLDEJsonMessage.toPath()), StandardCharsets.UTF_8);
-        EdxlMessage EDXLDEMessage = converter.deserializeJsonEDXL(EDXLDEJsonUseCase);
-        assertFalse(ConverterUtils.isCisuModel(EDXLDEMessage));
-
-        File CustomJsonMessage = new File(Objects.requireNonNull(TestMessagesHelper.class.getClassLoader().getResource("sample/valid/CustomContent/custom-content.json")).getFile());
-        String CustomUseCaseJson = new String(Files.readAllBytes(CustomJsonMessage.toPath()), StandardCharsets.UTF_8);
-        EdxlMessage CustomMessage = converter.deserializeJsonEDXL(CustomUseCaseJson);
-        assertFalse(ConverterUtils.isCisuModel(CustomMessage));
+        isCisuModelUtil("sample/examples/GEO-POS", false);
+        isCisuModelUtil("sample/examples/EMSI", false);
     }
 }
