24818671ea828432ab380a2e559f6312924bdd68
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-03-03 15:06:32 +0100

    feat(converter): implement downgrade to V1 (without medical notes fill)

--- converter/tests/test_v1_v2_converter.py
+++ converter/tests/test_v1_v2_converter.py
@@ -1,5 +1,5 @@
-from converter.converter.constants import Constants
-from converter.converter.v1_v2_converter import V1_V2Converter
+from converter.constants import Constants
+from converter.v1_v2_converter import V1_V2Converter
 from .test_helpers import TestHelper
 
 
@@ -16,4 +16,16 @@ def test_V1_to_V2_upgrade():
         converter_method=V1_V2Converter.upgrade,
         target_schema=v2_schema,
         online_tag=Constants.V1_GITHUB_TAG
+    )
+
+def test_V2_to_V1_downgrade():
+    v1_schema_endpoint = get_file_endpoint(Constants.V1_GITHUB_TAG)
+    v1_schema = TestHelper.load_json_file_online(v1_schema_endpoint)
+
+    TestHelper.conversion_tests_runner(
+        sample_dir=Constants.RS_EDA_TAG,
+        envelope_file=Constants.EDXL_HEALTH_TO_FIRE_ENVELOPE_PATH,
+        converter_method=V1_V2Converter.downgrade,
+        target_schema=v1_schema,
+        online_tag=Constants.V2_GITHUB_TAG
     )
