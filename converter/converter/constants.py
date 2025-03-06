4e03f32dd86c9a51306ee5e020c7a54a5f459949
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-03-06 14:21:46 +0100

    feat(converter): create edxl for health to health

--- converter/converter/constants.py
+++ converter/converter/constants.py
@@ -4,6 +4,7 @@ class Constants:
 
     EDXL_HEALTH_TO_FIRE_ENVELOPE_PATH="tests/edxl_envelope_health_to_fire.json"
     EDXL_FIRE_TO_HEALTH_ENVELOPE_PATH="tests/edxl_envelope_fire_to_health.json"
+    EDXL_HEALTH_TO_HEALTH_ENVELOPE_PATH="tests/edxl_envelope_health_to_health.json"
 
     RS_EDA_TAG="RS-EDA"
     RC_EDA_TAG="RC-EDA"
