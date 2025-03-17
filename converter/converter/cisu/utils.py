9155e3a1c8bbec7cf6be79c5187f40b5ac19643f
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-03-17 22:26:12 +0100

    feat(converter): mistakes in rebase

--- converter/converter/cisu/utils.py
+++ converter/converter/cisu/utils.py
@@ -1,25 +1,24 @@
 from typing import Any, Dict, List
 
 from yaml import dump
-from ..utils import is_field_completed, get_field_value
-
-
-def add_field_to_initial_alert_notes(data: Dict[str, Any], json_path: str):
-    field_value = get_field_value(data,json_path)
-
-    if field_value == None:
-        return
-
-    formatted_field_value = dump(field_value, allow_unicode=True)
-    add_object_to_initial_alert_notes(data, formatted_field_value)
-
-
-def add_to_initial_alert_notes(data: Dict[str, Any], paths: List[str]):
-    for path in paths:
-        add_field_to_initial_alert_notes(data, path)
+from ..utils import concatenate_values, get_field_value, is_field_completed
 
 def add_object_to_initial_alert_notes(json_data: Dict[str, Any], note_text: str):
     if not is_field_completed(json_data, '$.initialAlert.notes'):
         json_data['initialAlert']['notes'] = []
 
     json_data['initialAlert']['notes'].append({"freetext": note_text})
+
+def add_field_to_initial_alert_notes(data: Dict[str, Any], path_and_label: Dict[str, str]):
+    field_value = get_field_value(data,path_and_label['path'])
+
+    if field_value == None:
+        return
+
+    formatted_field_value = path_and_label['label']+ concatenate_values(field_value)
+    add_object_to_initial_alert_notes(data, formatted_field_value)
+
+
+def add_to_initial_alert_notes(data: Dict[str, Any], paths: List[Dict[str, str]]):
+    for path in paths:
+        add_field_to_initial_alert_notes(data, path)
