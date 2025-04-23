d0acf5b26355de5ad5d3b4b1e19d7e144e490ec5
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-04-23 12:10:41 +0200

    feat: write tests

--- converter/converter/constants.py
+++ converter/converter/constants.py
@@ -1,6 +1,7 @@
 class Constants:
     V1_GITHUB_TAG="v1.0.0-fix"
     V2_GITHUB_TAG="v2.0.0-fix"
+    V3_GITHUB_TAG="main"
 
     EDXL_HEALTH_TO_FIRE_ENVELOPE_PATH="tests/edxl_envelope_health_to_fire.json"
     EDXL_FIRE_TO_HEALTH_ENVELOPE_PATH="tests/edxl_envelope_fire_to_health.json"
