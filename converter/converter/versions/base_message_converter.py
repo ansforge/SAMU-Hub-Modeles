1344b11b15fd23e18d54235c865b586e784084fa
Clément Di Domenico <clement.didom@gmail.com>
2025-07-21 12:25:00 +0200

    chore(converter): fix base converter static methods definitions

--- converter/converter/versions/base_message_converter.py
+++ converter/converter/versions/base_message_converter.py
@@ -5,11 +5,11 @@ class BaseMessageConverter:
         raise ValueError("BaseMessageConverter is an abstract class and cannot be instantiated directly. Use a subclass instead.")
 
     @classmethod
-    def get_message_type() -> str:
+    def get_message_type(cls) -> str:
         raise NotImplementedError("Subclasses must implement this method to return the message type.")
 
     @classmethod
-    def convert(self, source_version, target_version, edxl_json):
+    def convert(cls, source_version, target_version, edxl_json):
         source_version_index = -1
         try:
             source_version_index = version_order_list.index(source_version)
@@ -30,66 +30,66 @@ class BaseMessageConverter:
             # Convert directly if version are consecutive
             if abs(source_version_index - target_version_index) == 1:
                 if source_version_index < target_version_index:
-                    return self.upgrade(source_version, source_version_index, edxl_json)
+                    return cls.upgrade(source_version, source_version_index, edxl_json)
                 else:
-                    return self.downgrade(source_version, source_version_index, edxl_json)
+                    return cls.downgrade(source_version, source_version_index, edxl_json)
 
             # Convert message to consecutive version
             version_delta = 1 if source_version_index < target_version_index else -1
-            converted_edxl_json = self.convert(
+            converted_edxl_json = cls.convert(
                 source_version,
                 version_order_list[source_version_index + version_delta],
                 edxl_json
             )
 
             # Recursively call convert with the new source_version
-            return self.convert(
+            return cls.convert(
                 version_order_list[source_version_index + version_delta],
                 target_version,
                 converted_edxl_json
             )
         except ValueError as err:
             print(f"[ERROR] {err}")
-            self.raise_conversion_not_implemented_error(source_version, target_version)
+            cls.raise_conversion_not_implemented_error(source_version, target_version)
 
     @classmethod
-    def upgrade(self, source_version, source_version_index, edxl_json):
+    def upgrade(cls, source_version, source_version_index, edxl_json):
         if source_version == "v1":
-            return self.convert_v1_to_v2(edxl_json)
+            return cls.convert_v1_to_v2(edxl_json)
         elif source_version == "v2":
-            return self.convert_v2_to_v3(edxl_json)
+            return cls.convert_v2_to_v3(edxl_json)
         else:
-            self.raise_conversion_impossible_error(source_version, version_order_list[source_version_index + 1])
+            cls.raise_conversion_impossible_error(source_version, version_order_list[source_version_index + 1])
 
     @classmethod
-    def downgrade(self, source_version, source_version_index, edxl_json):
+    def downgrade(cls, source_version, source_version_index, edxl_json):
         if source_version == "v2":
-            return self.convert_v2_to_v1(edxl_json)
+            return cls.convert_v2_to_v1(edxl_json)
         elif source_version == "v3":
-            return self.convert_v3_to_v2(edxl_json)
+            return cls.convert_v3_to_v2(edxl_json)
         else:
-            self.raise_conversion_impossible_error(source_version, version_order_list[source_version_index - 1])
+            cls.raise_conversion_impossible_error(source_version, version_order_list[source_version_index - 1])
 
     @classmethod
-    def convert_v1_to_v2(self, edxl_json):
-        self.raise_conversion_not_implemented_error("v1", "v2")
+    def convert_v1_to_v2(cls, edxl_json):
+        cls.raise_conversion_not_implemented_error("v1", "v2")
 
     @classmethod
-    def convert_v2_to_v1(self, edxl_json):
-        self.raise_conversion_not_implemented_error("v2", "v1")
+    def convert_v2_to_v1(cls, edxl_json):
+        cls.raise_conversion_not_implemented_error("v2", "v1")
 
     @classmethod
-    def convert_v2_to_v3(self, edxl_json):
-        self.raise_conversion_not_implemented_error("v2", "v3")
+    def convert_v2_to_v3(cls, edxl_json):
+        cls.raise_conversion_not_implemented_error("v2", "v3")
 
     @classmethod
-    def convert_v3_to_v2(self, edxl_json):
-        self.raise_conversion_not_implemented_error("v3", "v2")
+    def convert_v3_to_v2(cls, edxl_json):
+        cls.raise_conversion_not_implemented_error("v3", "v2")
 
     @classmethod
-    def raise_conversion_not_implemented_error(self, source_version, target_version):
-        raise ValueError(f"Version conversion from {source_version} to {target_version} for message type '{self.get_message_type()}' is currently not implemented.")
+    def raise_conversion_not_implemented_error(cls, source_version, target_version):
+        raise ValueError(f"Version conversion from {source_version} to {target_version} for message type '{cls.get_message_type()}' is currently not implemented.")
 
     @classmethod
-    def raise_conversion_impossible_error(self, source_version, target_version):
+    def raise_conversion_impossible_error(cls, source_version, target_version):
         raise ValueError(f"Version conversion from {source_version} to {target_version} is not possible.")
