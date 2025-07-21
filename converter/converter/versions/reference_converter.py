5ddb5f44c2bf25b266a2c11382d338dba3b2ca5c
Clément Di Domenico <clement.didom@gmail.com>
2025-07-21 12:25:20 +0200

    refactor(converter): identical message converter

--- converter/converter/versions/reference_converter.py
+++ converter/converter/versions/reference_converter.py
@@ -1,24 +1,6 @@
-from typing import Dict, Any
+from converter.versions.identical_message_converter import IdenticalMessageConverter
 
-from converter.versions.base_message_converter import BaseMessageConverter
-
-class ReferenceConverter(BaseMessageConverter):
+class ReferenceConverter(IdenticalMessageConverter):
     @staticmethod
     def get_message_type():
         return "reference"
-
-    @classmethod
-    def convert_v1_to_v2(self, input_json) -> Dict[str, Any]:
-        return input_json
-
-    @classmethod
-    def convert_v2_to_v1(self, input_json: Dict[str, Any]) -> Dict[str, Any]:
-        return input_json
-
-    @classmethod
-    def convert_v2_to_v3(self, input_json) -> Dict[str, Any]:
-        return input_json
-
-    @classmethod
-    def convert_v3_to_v2(self, input_json: Dict[str, Any]) -> Dict[str, Any]:
-        return input_json
