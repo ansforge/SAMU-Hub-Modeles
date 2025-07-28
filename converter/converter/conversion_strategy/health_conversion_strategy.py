3a46549504025b7b3508c54d611fc800aad819f5
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-07-28 15:13:58 +0200

    feat: use new converters :)

--- converter/converter/conversion_strategy/health_conversion_strategy.py
+++ converter/converter/conversion_strategy/health_conversion_strategy.py
@@ -5,6 +5,8 @@ from converter.versions.error_converter import ErrorConverter
 from converter.versions.reference_converter import ReferenceConverter
 from converter.versions.create_case_health.create_case_health_converter import CreateHealthCaseConverter
 from converter.versions.resources_info.resources_info_converter import ResourcesInfoConverter
+from converter.versions.resources_request.resources_request_converter import ResourcesRequestConverter
+from converter.versions.resources_response.resources_response_converter import ResourcesResponseConverter
 from converter.versions.resources_status.resources_status_converter import ResourcesStatusConverter
 
 def health_conversion_strategy(edxl_json, source_version: str, target_version: str):
@@ -23,6 +25,10 @@ def health_conversion_strategy(edxl_json, source_version: str, target_version: s
         return ResourcesStatusConverter.convert(source_version, target_version, edxl_json)
     elif 'resourcesInfo' in message_content:
         return ResourcesInfoConverter.convert(source_version, target_version, edxl_json)
+    elif 'resourcesResponse' in message_content:
+        return ResourcesResponseConverter.convert(source_version, target_version, edxl_json)
+    elif 'resourcesRequest' in message_content:
+        return ResourcesRequestConverter.convert(source_version, target_version, edxl_json)
     else:
         deducted_message_type = extract_message_type_from_message_content(message_content)
         raise ValueError(f"Version conversion from {source_version} to {target_version} for message type '{deducted_message_type}' is currently not implemented.")
