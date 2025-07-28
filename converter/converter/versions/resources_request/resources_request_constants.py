dc07d808e2798efe606e78ecd5e7cf72695acf86
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-07-28 14:53:54 +0200

    feat: add conversion logic for RS-DR

---
+++ converter/converter/versions/resources_request/resources_request_constants.py
@@ -0,0 +1,14 @@
+class ResourcesRequestConstants:
+    V1_TO_V2_DEADLINE_MAPPING = {
+        "DEL0": "DEL0",
+        "ASAP": "ASAP",
+        "DEL30M": "30M",
+        "DEL45M": "45M",
+        "DEL1H": "1H",
+        "DEL2H": "2H",
+        "DEL4H": "4H",
+        "DEL8H": "8H",
+        "DEL12H": "12H",
+        "DEL24H": "24H",
+        "RDV": "RDV"
+    }
---
+++ converter/converter/versions/resources_request/resources_request_converter.py
@@ -0,0 +1,58 @@
+import copy
+from typing import Dict, Any
+
+from converter.utils import map_to_new_value
+from converter.versions.base_message_converter import BaseMessageConverter
+from converter.versions.resources_request.resources_request_constants import ResourcesRequestConstants
+from converter.versions.utils import reverse_map_to_new_value
+
+class ResourcesRequestConverter(BaseMessageConverter):
+    @staticmethod
+    def get_message_type():
+        return "resourcesRequest"
+
+    @classmethod
+    def copy_input_content(cls, input_json: Dict[str, Any]) -> Dict[str, Any]:
+        # Create independent envelope copy without use case for output
+        output_json = copy.deepcopy(input_json)
+        if cls.get_message_type() not in input_json.get('content', [{}])[0].get('jsonContent', {}).get('embeddedJsonContent', {}).get('message', {}):
+            raise ValueError(f"Input JSON must contain {cls.get_message_type()} key")
+        del output_json['content'][0]['jsonContent']['embeddedJsonContent']['message'][cls.get_message_type()]
+        return output_json
+
+    @classmethod
+    def copy_input_use_case_content(cls, input_json: Dict[str, Any]) -> Dict[str, Any]:
+        # Create independent use case copy for output
+        input_use_case_json = input_json['content'][0]['jsonContent']['embeddedJsonContent']['message'][cls.get_message_type()]
+        return copy.deepcopy(input_use_case_json)
+
+    @classmethod
+    def format_output_json(cls, output_json: Dict[str, Any], output_use_case_json: Dict[str, Any]) -> Dict[str, Any]:
+        output_json['content'][0]['jsonContent']['embeddedJsonContent']['message'][cls.get_message_type()] = output_use_case_json
+        return output_json
+
+    @classmethod
+    def convert_v1_to_v2(cls, input_json) -> Dict[str, Any]:
+        output_json = cls.copy_input_content(input_json)
+        output_use_case_json = cls.copy_input_use_case_content(input_json)
+
+        map_to_new_value(output_use_case_json,'$.request.deadline', ResourcesRequestConstants.V1_TO_V2_DEADLINE_MAPPING)
+
+        return cls.format_output_json(output_json, output_use_case_json)
+
+    @classmethod
+    def convert_v2_to_v1(cls, input_json: Dict[str, Any]) -> Dict[str, Any]:
+        output_json = cls.copy_input_content(input_json)
+        output_use_case_json = cls.copy_input_use_case_content(input_json)
+
+        reverse_map_to_new_value(output_use_case_json,'$.request.deadline', ResourcesRequestConstants.V1_TO_V2_DEADLINE_MAPPING)
+
+        return cls.format_output_json(output_json, output_use_case_json)
+
+    @classmethod
+    def convert_v2_to_v3(cls, input_json) -> Dict[str, Any]:
+        return input_json
+
+    @classmethod
+    def convert_v3_to_v2(cls, input_json: Dict[str, Any]) -> Dict[str, Any]:
+        return input_json
