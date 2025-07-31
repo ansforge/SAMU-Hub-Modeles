8e22f0cb3fbfa3a6462569fbf3a56a3a2c9d3e53
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-07-31 09:27:08 +0200

    feat: add conversion rules for RC-REF

---
+++ converter/converter/versions/reference/reference_converter.py
@@ -0,0 +1,18 @@
+from converter.utils import delete_paths
+from converter.versions.conversion_mixin import ConversionMixin
+from converter.versions.identical_message_converter import IdenticalMessageConverter
+from converter.versions.reference.reference_constants import ReferenceConstants
+
+class ReferenceConverter(IdenticalMessageConverter, ConversionMixin):
+    @staticmethod
+    def get_message_type():
+        return "reference"
+
+    @classmethod
+    def convert_v3_to_v2(cls, input_json):
+        output_json = cls.copy_input_content(input_json)
+        output_use_case_json = cls.copy_input_use_case_content(input_json)
+
+        delete_paths(output_use_case_json, ReferenceConstants.V3_PATHS_TO_DELETE)
+
+        return cls.format_output_json(output_json, output_use_case_json)
--- converter/converter/versions/reference_converter.py
+++
@@ -1,6 +0,0 @@
-from converter.versions.identical_message_converter import IdenticalMessageConverter
-
-class ReferenceConverter(IdenticalMessageConverter):
-    @staticmethod
-    def get_message_type():
-        return "reference"
