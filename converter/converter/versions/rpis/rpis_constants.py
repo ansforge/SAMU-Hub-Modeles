b826f3cfd99955fa2f1bbd647410f4457abc16e1
SimonClo <simon.cloitre@hotmail.fr>
2025-10-15 15:21:52 +0200

    feat(converter): rpis conversion strategy

---
+++ converter/converter/versions/rpis/rpis_constants.py
@@ -0,0 +1,6 @@
+class RpisConstants:
+    RESOURCE_TYPE_PATH = "$.orientation.decision.resourceType"
+    V3_TO_V2_RESOURCE_TYPE_MAPPING = {"TSU": "TSU "}
+
+    MEDICAL_LEVEL_PATH = "$.regulation.medicalLevel"
+    V3_TO_V2_MEDICAL_LEVEL_MAPPING = {"SANS": "MED"}
