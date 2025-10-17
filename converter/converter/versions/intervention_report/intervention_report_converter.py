7b0b8873990d2a7270315276cfd19eaef5cda412
SimonClo <simon.cloitre@hotmail.fr>
2025-10-17 17:27:28 +0200

    feat(converter): intervention report default strategy

---
+++ converter/converter/versions/intervention_report/intervention_report_converter.py
@@ -0,0 +1,23 @@
+from typing import Dict, Any
+
+from converter.versions.conversion_mixin import ConversionMixin
+
+
+class InterventionReportConverter(ConversionMixin):
+    @staticmethod
+    def get_message_type():
+        return "interventionReport"
+
+    @classmethod
+    def convert_v2_to_v3(cls, input_json) -> Dict[str, Any]:
+        output_json = cls.copy_input_content(input_json)
+        output_use_case_json = cls.copy_input_use_case_content(input_json)
+
+        return cls.format_output_json(output_json, output_use_case_json)
+
+    @classmethod
+    def convert_v3_to_v2(cls, input_json: Dict[str, Any]) -> Dict[str, Any]:
+        output_json = cls.copy_input_content(input_json)
+        output_use_case_json = cls.copy_input_use_case_content(input_json)
+
+        return cls.format_output_json(output_json, output_use_case_json)
