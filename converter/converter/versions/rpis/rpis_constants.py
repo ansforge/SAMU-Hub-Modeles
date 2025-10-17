f34e161ff19f3931430980006dc16e341c611a30
SimonClo <simon.cloitre@hotmail.fr>
2025-10-17 14:05:33 +0200

    feat(converter): conversion for medicalLevel in orientation section

--- converter/converter/versions/rpis/rpis_constants.py
+++ converter/converter/versions/rpis/rpis_constants.py
@@ -2,5 +2,6 @@ class RpisConstants:
     RESOURCE_TYPE_PATH = "$.orientation.decision.resourceType"
     V3_TO_V2_RESOURCE_TYPE_MAPPING = {"TSU": "TSU "}
 
-    MEDICAL_LEVEL_PATH = "$.regulation.medicalLevel"
+    REGULATION_MEDICAL_LEVEL_PATH = "$.regulation.medicalLevel"
+    ORIENTATION_MEDICAL_LEVEL_PATH = "$.orientation.decision.medicalLevel"
     V3_TO_V2_MEDICAL_LEVEL_MAPPING = {"SANS": "MED"}
--- converter/tests/versions/test_rpis_converter.py
+++ converter/tests/versions/test_rpis_converter.py
@@ -46,7 +46,7 @@ def test_v2_to_v3_upgrade_tsu():
     v3_schema = get_v3_schema()
     message_raw_v2 = TestHelper.create_edxl_json_from_sample(
         TestConstants.EDXL_HEALTH_TO_HEALTH_ENVELOPE_PATH,
-        "tests/fixtures/RS-RPIS/RS-RPIS_V2.0_TSU.json",
+        "tests/fixtures/RS-RPIS/RS-RPIS_V2.0_resourceType_TSU.json",
     )
     message_raw_v3 = RpisConverter.convert_v2_to_v3(message_raw_v2)
     message_v3 = RpisConverter.copy_input_use_case_content(message_raw_v3)
@@ -58,7 +58,7 @@ def test_v3_to_v2_downgrade_tsu_sans():
     v2_schema = get_v2_schema()
     message_raw_v3 = TestHelper.create_edxl_json_from_sample(
         TestConstants.EDXL_HEALTH_TO_HEALTH_ENVELOPE_PATH,
-        "tests/fixtures/RS-RPIS/RS-RPIS_V3.0_TSU_SANS.json",
+        "tests/fixtures/RS-RPIS/RS-RPIS_V3.0_resourceType_TSU_medicalLevel_SANS.json",
     )
     message_raw_v2 = RpisConverter.convert_v3_to_v2(message_raw_v3)
     message_v2 = RpisConverter.copy_input_use_case_content(message_raw_v2)
