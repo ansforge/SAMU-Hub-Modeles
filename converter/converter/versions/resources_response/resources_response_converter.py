8e9acdaf173f0bbda1033d6b1a4ec9cd14bc0120
SimonClo <simon.cloitre@hotmail.fr>
2025-12-15 14:59:57 +0100

    refactor(converter): use standardized modification functions in resources response and status converter

--- converter/converter/versions/resources_response/resources_response_converter.py
+++ converter/converter/versions/resources_response/resources_response_converter.py
@@ -1,6 +1,6 @@
 from typing import Dict, Any
 
-from converter.utils import get_field_value
+from converter.utils import get_field_value, set_value
 from converter.versions.identical_message_converter import IdenticalMessageConverter
 from converter.versions.resources_response.resources_response_constants import (
     ResourcesResponseConstants,
@@ -17,13 +17,17 @@ class ResourcesResponseConverter(IdenticalMessageConverter):
         output_json = cls.copy_input_content(input_json)
         output_use_case_json = cls.copy_input_use_case_content(input_json)
 
-        deadline = get_field_value(output_use_case_json, "$.response.deadline")
+        deadline = get_field_value(
+            output_use_case_json, ResourcesResponseConstants.RESPONSE_DEADLINE_PATH
+        )
         if (
             deadline is not None
             and deadline not in ResourcesResponseConstants.V2_VALID_DEADLINES
         ):
-            output_use_case_json["response"]["deadline"] = (
-                ResourcesResponseConstants.V2_DEFAULT_DEADLINE
+            set_value(
+                output_use_case_json,
+                ResourcesResponseConstants.RESPONSE_DEADLINE_PATH,
+                ResourcesResponseConstants.V2_DEFAULT_DEADLINE,
             )
 
         return cls.format_output_json(output_json, output_use_case_json)
