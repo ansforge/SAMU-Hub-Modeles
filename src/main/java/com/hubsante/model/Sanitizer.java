e7ac6158567d50c7e6557f0043a716e6f42da5b2
Issam Kadar <issam@kadar.pro>
2026-01-12 18:21:07 +0100

    chore : update licence

--- src/main/java/com/hubsante/model/Sanitizer.java
+++ src/main/java/com/hubsante/model/Sanitizer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2023-2025 Agence du Numerique en Sante (ANS)
+ * Copyright © 2023-2026 Agence du Numerique en Sante (ANS)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
