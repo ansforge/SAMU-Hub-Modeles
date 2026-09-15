b2b869ea39b8fca87dc9a7f3e1bc57b113186dc7
Issam KADAR <issam@kadar.pro>
2026-09-15 10:40:31 +0200

    feature(converter) : disable strip for vactive to v3

--- converter/converter/cisu_version_converters/reference/reference_version_converter.py
+++ converter/converter/cisu_version_converters/reference/reference_version_converter.py
@@ -3,28 +3,19 @@ from typing import Any, Dict
 from converter.cisu_transcoders.reference.reference_cisu_constants import (
     ReferenceCISUConstants,
 )
-from converter.cisu_version_converters.base_cisu_version_converter import (
-    BaseCISUVersionConverter,
+from converter.cisu_version_converters.identical_cisu_version_converter import (
+    IdenticalCISUVersionConverter,
 )
 from converter.utils import delete_paths
 
 
-class ReferenceCISUVersionConverter(BaseCISUVersionConverter):
+class ReferenceCISUVersionConverter(IdenticalCISUVersionConverter):
     @classmethod
     def get_message_type(cls) -> str:
         return "reference"
 
     @classmethod
     def convert_v3_to_vactive(cls, edxl_json: Dict[str, Any]) -> Dict[str, Any]:
-        output_json = cls.copy_input_content(edxl_json)
-        output_use_case_json = cls.copy_input_use_case_content(edxl_json)
-
-        delete_paths(output_use_case_json, ReferenceCISUConstants.CISU_PATHS_TO_DELETE)
-
-        return cls.format_output_json(output_json, output_use_case_json)
-
-    @classmethod
-    def convert_vactive_to_v3(cls, edxl_json: Dict[str, Any]) -> Dict[str, Any]:
         output_json = cls.copy_input_content(edxl_json)
         output_use_case_json = cls.copy_input_use_case_content(edxl_json)
 
